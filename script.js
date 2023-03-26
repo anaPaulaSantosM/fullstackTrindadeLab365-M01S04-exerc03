da8572588974bc405867026d1726d1694f74eec9
Ana Paulo dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-03-26 12:24:04 -0300

    atualizando class

--- script.js
+++ script.js
@@ -31,9 +31,10 @@ class Conta {
   }
 
   class ContaPoupanca extends Conta {
-    get saldo() {
-      return this.saldo;
-    }
+      constructor (saldo){
+        super(saldo);
+      }
+
     atualizaJuros(saldo){
       this.saldoAtualizado = (saldo * 0.70)/100 + saldo;
     }
@@ -43,7 +44,6 @@ class Conta {
   //contaCorrente.deposito(200, 1234);
   //contaCorrente.retirada(80);
   //console.log(contaCorrente.saldo);
-  const contaPoupanca = new ContaPoupanca(0, 1234);
-  contaPoupanca.deposito(200,1234);
-  contaPoupanca.atualizaJuros(400);
+  const contaPoupanca = new ContaPoupanca(0,1234);
+  contaPoupanca.atualizaJuros(300);
   console.log(contaPoupanca.saldoAtualizado);
